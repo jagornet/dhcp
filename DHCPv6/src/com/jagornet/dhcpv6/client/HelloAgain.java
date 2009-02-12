3dac4b5a9e4da5c1bd7f7bb6e567ded60b75bbb3
agrabil <agrabil@b93dd69a-310d-e06c-c708-a6fd4d7b29e7>
2009-02-12 01:10:59 +0000

    hello test

---
+++ DHCPv6/src/com/jagornet/dhcpv6/client/HelloAgain.java
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ * 
+ * Copyright 2009 Jagornet Technologies, LLC.  All Rights Reserved.
+ *
+ * This software is the proprietary information of Jagornet Technologies, LLC. 
+ * Use is subject to license terms.
+ *
+ */
+package com.jagornet.dhcpv6.client;
+
+/**
+ * @author agrabil
+ *
+ */
+public class HelloAgain {
+
+}
