e67f78e84a3d20e93a3d5a87c78974c6d917fcfe
agrabil <greg.rabil@jagornet.com>
2017-07-31 09:30:42 -0400

    Initial commit for 2.1 release.
    Refactor as Maven project.
    Update to Java 8.
    Replace XMLBeans implementation with JAXB2.
    Update versions of third party libraries.

---
+++ Jagornet-DHCP/src/main/test/com/jagornet/dhcp/server/config/TestDhcpServerConfiguration.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2009-2014 Jagornet Technologies, LLC.  All Rights Reserved.
+ *
+ * This software is the proprietary information of Jagornet Technologies, LLC. 
+ * Use is subject to license terms.
+ *
+ */
+
+/*
+ *   This file TestDhcpServerConfiguration.java is part of Jagornet DHCP.
+ *
+ *   Jagornet DHCP is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Jagornet DHCP is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Jagornet DHCP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.jagornet.dhcp.server.config;
+
+import java.net.InetAddress;
+import java.util.SortedMap;
+
+import com.jagornet.dhcp.option.OpaqueDataUtil;
+import com.jagornet.dhcp.util.DhcpConstants;
+import com.jagornet.dhcp.util.Subnet;
+import com.jagornet.dhcp.util.Util;
+import com.jagornet.dhcp.xml.DhcpServerConfig;
+import com.jagornet.dhcp.xml.OpaqueData;
+import com.jagornet.dhcp.xml.PoliciesType;
+import com.jagornet.dhcp.xml.Policy;
+import com.jagornet.dhcp.xml.V6ServerIdOption;
+
+import junit.framework.TestCase;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TestDhcpServerConfiguration.
+ */
+public class TestDhcpServerConfiguration extends TestCase
+{
+	
+	/**
+	 * Test save and load config.
+	 * 
+	 * @throws Exception the exception
+	 */
+	public void testSaveAndLoadConfig() throws Exception
+	{
+		DhcpServerConfig config = new DhcpServerConfig();
+		
+		V6ServerIdOption serverId = new V6ServerIdOption();
+		OpaqueData duid = OpaqueDataUtil.generateDUID_LLT();
+		serverId.setOpaqueData(duid);
+		config.setV6ServerIdOption(serverId);
+		
+		Policy policy = new Policy();
+		policy.setName("sendRequestedOptionsOnly");
+		policy.setValue("true");
+		PoliciesType policies = new PoliciesType();
+		policies.getPolicy().add(policy);
+		config.setPolicies(policies);
+		
+		DhcpServerConfiguration.saveConfig(config, "test/com/jagornet/dhcp/server/config/dhcpServerConfigTestSave.xml");
+		
+		config = DhcpServerConfiguration.loadConfig("test/com/jagornet/dhcp/server/config/dhcpServerConfigTestSave.xml");
+		assertNotNull(config);
+		assertNotNull(config.getV6ServerIdOption());
+		assertNotNull(config.getPolicies().getPolicy());
+		assertEquals(1, config.getPolicies().getPolicy().size());
+		assertEquals("sendRequestedOptionsOnly", config.getPolicies().getPolicy().get(0).getName());
+		assertEquals("true", config.getPolicies().getPolicy().get(0).getValue());
+	}
+	
+    /**
+     * Test link map.
+     * 
+     * @throws Exception the exception
+     */
+    public void testLinkMap() throws Exception
+    {
+    	String configFilename = "test/com/jagornet/dhcp/server/config/dhcpServerConfigLinkTest1.xml";
+    	DhcpServerConfiguration.configFilename = configFilename;
+        DhcpServerConfiguration serverConfig = DhcpServerConfiguration.getInstance();
+        
+        DhcpServerConfig config = serverConfig.getDhcpServerConfig();
+        assertNotNull(config);
+		assertNotNull(config.getPolicies().getPolicy());
+		assertEquals(1, config.getPolicies().getPolicy().size());
+		assertEquals("sendRequestedOptionsOnly", config.getPolicies().getPolicy().get(0).getName());
+		assertEquals("true", config.getPolicies().getPolicy().get(0).getValue());
+        assertNotNull(config.getV6ServerIdOption());
+		assertEquals(DhcpConstants.V6OPTION_SERVERID, config.getV6ServerIdOption().getCode());
+        assertEquals("abcdef0123456789", Util.toHexString(config.getV6ServerIdOption().getOpaqueData().getHexValue()));
+        assertNotNull(config.getV6MsgConfigOptions().getV6DnsServersOption());
+        assertEquals(DhcpConstants.V6OPTION_DNS_SERVERS, config.getV6MsgConfigOptions().getV6DnsServersOption().getCode());
+        assertEquals(3, config.getV6MsgConfigOptions().getV6DnsServersOption().getIpAddress().size());
+        assertEquals("3ffe::0001", config.getV6MsgConfigOptions().getV6DnsServersOption().getIpAddress().get(0));
+        assertEquals("3ffe::0002", config.getV6MsgConfigOptions().getV6DnsServersOption().getIpAddress().get(1));
+        assertEquals("3ffe::0003", config.getV6MsgConfigOptions().getV6DnsServersOption().getIpAddress().get(2));
+        assertNotNull(config.getV6MsgConfigOptions().getV6DomainSearchListOption());
+        assertEquals(DhcpConstants.V6OPTION_DOMAIN_SEARCH_LIST, config.getV6MsgConfigOptions().getV6DomainSearchListOption().getCode());
+        assertEquals(3, config.getV6MsgConfigOptions().getV6DomainSearchListOption().getDomainName().size());
+        assertEquals("foo.com.", config.getV6MsgConfigOptions().getV6DomainSearchListOption().getDomainName().get(0));
+        assertEquals("bar.com.", config.getV6MsgConfigOptions().getV6DomainSearchListOption().getDomainName().get(1));
+        assertEquals("yuk.com.", config.getV6MsgConfigOptions().getV6DomainSearchListOption().getDomainName().get(2));
+        
+        SortedMap<Subnet, DhcpLink> linkMap = serverConfig.getLinkMap();
+        assertNotNull(linkMap);
+        assertEquals(5, linkMap.size());
+        Subnet searchAddr = new Subnet("2001:DB8:3:1:DEB:DEB:DEB:1", 128);
+        // there are two subnets greater than our search address
+        SortedMap<Subnet, DhcpLink> subMap = linkMap.tailMap(searchAddr);
+        assertNotNull(subMap);
+        assertEquals(2, subMap.size());
+
+        // there are three subnets less than our search address
+        subMap = linkMap.headMap(searchAddr);
+        assertNotNull(subMap);
+        assertEquals(3, subMap.size());
+        // the last subnet from the head list is the one we want
+        assertEquals(InetAddress.getByName("2001:DB8:3::"), 
+                     subMap.lastKey().getSubnetAddress());
+    }
+}
---
+++ Jagornet-DHCP/src/main/test/com/jagornet/dhcp/util/TestLinkMap.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2009-2014 Jagornet Technologies, LLC.  All Rights Reserved.
+ *
+ * This software is the proprietary information of Jagornet Technologies, LLC. 
+ * Use is subject to license terms.
+ *
+ */
+
+/*
+ *   This file TestLinkMap.java is part of Jagornet DHCP.
+ *
+ *   Jagornet DHCP is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Jagornet DHCP is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Jagornet DHCP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.jagornet.dhcp.util;
+
+import java.net.InetAddress;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import junit.framework.TestCase;
+
+import com.jagornet.dhcp.server.config.DhcpLink;
+import com.jagornet.dhcp.util.Subnet;
+import com.jagornet.dhcp.xml.Link;
+
+public class TestLinkMap extends TestCase 
+{
+    private SortedMap<Subnet, DhcpLink> linkMap;
+    
+    @Override
+    protected void setUp() throws Exception
+    {
+    	super.setUp();
+    	linkMap = new TreeMap<Subnet, DhcpLink>();
+        String addr = "2001:DB8:1::/64";
+        String[] s = addr.split("/");
+        if ((s != null) && (s.length == 2)) {
+            Subnet subnet = new Subnet(s[0], s[1]);
+            linkMap.put(subnet, new DhcpLink(subnet, new Link()));
+        }
+        addr = "2001:DB8:1:1::/64";
+        s = addr.split("/");
+        if ((s != null) && (s.length == 2)) {
+            Subnet subnet = new Subnet(s[0], s[1]);
+            linkMap.put(subnet, new DhcpLink(subnet, new Link()));
+        }
+        addr = "2001:DB8:1:2::/64";
+        s = addr.split("/");
+        if ((s != null) && (s.length == 2)) {
+            Subnet subnet = new Subnet(s[0], s[1]);
+            linkMap.put(subnet, new DhcpLink(subnet, new Link()));
+        }
+    }
+    
+    public void testFindSubnetAddress() throws Exception
+    {
+    	InetAddress addr = InetAddress.getByName("2001:DB8:1:1:0:0:0:0");
+    	assertNotNull(findLink(addr));
+    }
+    
+    public void testFindAddress() throws Exception
+    {
+    	InetAddress addr = InetAddress.getByName("2001:DB8:1:1:0:0:0:1");
+    	assertNotNull(findLink(addr));
+    }
+    
+    private DhcpLink findLink(InetAddress addr) throws Exception
+    {
+        Subnet s = new Subnet(addr, 128);
+        // find links less than the given address
+        SortedMap<Subnet, DhcpLink> subMap = linkMap.headMap(s);
+        if ((subMap != null) && !subMap.isEmpty()) {
+            Subnet k = subMap.lastKey();
+            if (k.contains(addr)) {
+                return subMap.get(k);
+            }
+        }
+        // find links greater or equal to given address
+        subMap = linkMap.tailMap(s);
+        if ((subMap != null) && !subMap.isEmpty()) {
+            Subnet k = subMap.firstKey();
+            if (k.contains(addr)) {
+                return subMap.get(k);
+            }
+        }
+        return null;
+    }
+}
