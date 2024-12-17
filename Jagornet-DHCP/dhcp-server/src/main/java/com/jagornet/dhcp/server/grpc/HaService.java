77e202528e94da18cc2d07c1795e79c171ae8fee
agrabil <greg.rabil@jagornet.com>
2024-12-16 21:18:05 -0500

    Integration testing updates
    - fix typo in getLeases endIp
    - fix updateLease to invoke responseObserver methods as needed for the gRPC implementation
    - add debug messages

--- Jagornet-DHCP/dhcp-server/src/main/java/com/jagornet/dhcp/server/grpc/HaService.java
+++ Jagornet-DHCP/dhcp-server/src/main/java/com/jagornet/dhcp/server/grpc/HaService.java
@@ -1,7 +1,6 @@
 package com.jagornet.dhcp.server.grpc;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,25 +48,42 @@ public class HaService extends HaServiceImplBase {
     @Override
     public void updateLease(DhcpLeaseUpdate request, StreamObserver<DhcpLeaseUpdate> responseObserver) {
         DhcpLease lease = DhcpLeaseUtil.grpcToDhcpLease(request);
-        // ?? leasesService.createOrUpdateDhcpLease(lease); 
-        leasesService.updateDhcpLease(lease.getIpAddress(), lease);
+        log.debug("Update lease: " + lease);
+        // ?? leasesService.createOrUpdateDhcpLease(lease);
+        if (leasesService.updateDhcpLease(lease.getIpAddress(), lease)) {
+            // if the update succeeded, then return the request lease
+            responseObserver.onNext(request);
+            responseObserver.onCompleted();
+        }
+        else {
+            responseObserver.onError(new IllegalStateException("updateLease failed"));
+        }
     }
 
     @Override
     public void getLeases(DhcpLeasesRequest request, StreamObserver<DhcpLeaseUpdate> responseObserver) {
         try {
             InetAddress startIp = InetAddress.getByAddress(request.getStartIpAddress().toByteArray());
-            InetAddress endIp = InetAddress.getByAddress(request.getStartIpAddress().toByteArray());
+            InetAddress endIp = InetAddress.getByAddress(request.getEndIpAddress().toByteArray());
             boolean unsyncedLeasesOnly = request.getUnsyncedLeasesOnly();
+            log.info("Getting leases for range:"
+                        + " start=" + startIp.getHostAddress()
+                        + " end=" + endIp.getHostAddress()
+                        + " unsyncedLeasesOnly=" + unsyncedLeasesOnly);
             leasesService.getRangeLeases(startIp, endIp, new DhcpLeaseCallbackHandler() {
-
                 @Override
                 public void processDhcpLease(DhcpLease dhcpLease) throws Exception {
+                    log.debug("Next dhcpLease: " + dhcpLease);
                     DhcpLeaseUpdate leaseUpdate = DhcpLeaseUtil.dhcpLeaseToGrpc(dhcpLease);
                     responseObserver.onNext(leaseUpdate);
                 }
             }, unsyncedLeasesOnly);
             // are we really done here, or does this continue asychronously?
+            // TODO: no, it doesn't continue aysync, so we have to wait...
+            log.info("Completed getting leases for range:"
+                        + " start=" + startIp.getHostAddress()
+                        + " end=" + endIp.getHostAddress()
+                        + " unsyncedLeasesOnly=" + unsyncedLeasesOnly);
             responseObserver.onCompleted();
         }
         catch (Exception e) {
